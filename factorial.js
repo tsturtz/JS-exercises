4aa9a0b8aa9e7af14df7ce782ba7d0ff77b6b1a2
taylor_sturtz <taylorsturtz@gmail.com>
2017-02-13 23:50:18 -0800

    factorial loop - next step would be to cache results for large number calculations

--- factorial.js
+++ factorial.js
@@ -1,6 +1,6 @@
 // Factorial of any number n is defined as the multiplication of numbers from (1) to the given number.
 
-// Recursive
+// Recursive (call-stack overflow)
 function factorialRecurse(num) {
   if (num === 1) {
     return num;
@@ -9,3 +9,15 @@ function factorialRecurse(num) {
 }
 var factorialRecurseResult = factorialRecurse(4);
 console.log(factorialRecurseResult);
+
+// Iterative (faster, but next step would be to cache results to manage large number calculations)
+function factorialLoop(num) {
+  var result = num;
+  while (num > 1) {
+    result = result * (num - 1);
+    num--;
+  }
+  return result;
+}
+var factorialLoopResult = factorialLoop(9);
+console.log(factorialLoopResult);
