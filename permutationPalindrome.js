c18a4366e9c82a7114d5d297e013de47924db81f
taylor_sturtz <taylorsturtz@gmail.com>
2017-02-24 22:39:23 -0800

    adding Permutation Palindrome - question from InterviewCake, to solve later

---
+++ permutationPalindrome.js
@@ -0,0 +1,11 @@
+// InterviewCake #30 - Permutation Palindrome
+// Write an efficient function that checks whether any permutation of an input string is a palindrome.
+//
+// Examples:
+// "civic" should return true
+// "ivicc" should return true
+// "civil" should return false
+// "livci" should return false
+//
+// "But 'ivicc' isn't a palindrome!"
+// If you had this thought, read the question again carefully. We're asking if any permutation of the string is a palindrome. Spend some extra time ensuring you fully understand the question before starting. Jumping in with a flawed understanding of the problem doesn't look good in an interview.
