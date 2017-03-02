50380d805417c1da101c5a75acc1ad2f40651f2e
taylor_sturtz <taylorsturtz@gmail.com>
2017-03-01 17:49:45 -0800

    cleaned up perm-palindrome a bit and added parenthesis matching

--- permutationPalindrome.js
+++ permutationPalindrome.js
@@ -9,8 +9,8 @@
 
 function permutationPalindrome(str) {
   var obj = {}, countOdd = 0;
-  // set string to lower case; remove white space and all non-letters
-  str = str.toLowerCase().replace(/\s+/g,'').replace(/\W+/g,'').replace(/\d+/g,'');
+  // set string to lower case; remove all non-letters (whitespace, digits, special characters, etc.)
+  str = str.toLowerCase().replace(/[^a-zA-Z]/g,'');
   for (var i = 0; i < str.length; i++) {
     // if the property at str[i] does not exist in obj, set it to 0
     if (!obj[str[i]]) obj[str[i]] = 0;
@@ -19,17 +19,22 @@ function permutationPalindrome(str) {
   }
   for (var x in obj) {
     // if the count of occurances of a character is odd, increment the odd counter
-  	if ((obj[x] % 2) > 0) countOdd++;
+  	if ((obj[x] % 2) !== 0) countOdd++;
   }
   // if count of odd characters is more than 1, it cannot become a palindrome
-  if (countOdd > 1) return str + ' cannot become a palindrome';
+  if (countOdd > 1) return str + ' : (FALSE) no permutation makes a palindrome';
   // else, it can!
-  else return str + ' is or can become a palindrome';
+  else return str + ' : (TRUE) palindrome present';
 }
 
 console.log(permutationPalindrome('civic')); // true
 console.log(permutationPalindrome('ivicc')); // true
+
 console.log(permutationPalindrome('civil')); // false
 console.log(permutationPalindrome('livci')); // false
+
 console.log(permutationPalindrome('racecar')); // true
+console.log(permutationPalindrome('c;!ar race-')); // true
+console.log(permutationPalindrome('racecard')); // false
+
 console.log(permutationPalindrome('A man, a plan, a cat, a canal -- Panama')); // true
