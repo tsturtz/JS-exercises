93ddf5e0785e38a4aee630a072305b946eaf4f8f
taylor_sturtz <taylorsturtz@gmail.com>
2017-03-04 21:47:25 -0800

    parenthesis matching completed

--- parenthesisMatching.js
+++ parenthesisMatching.js
@@ -6,3 +6,29 @@
 // Write a function that, given a sentence like the one above, along with the position of an opening parenthesis, finds the corresponding closing parenthesis.
 //
 // Example: if the example string above is input with the number 10 (position of the first parenthesis), the output should be 79 (position of the last parenthesis).
+
+function parenMatch (str) {
+	// Get position of first open paren and set open parens to 0;
+  var parenIdx = str.indexOf('('), openParens = 0;
+	// Check if there are any parens
+	if (parenIdx > -1) {
+		for (parenIdx; parenIdx < str.length; parenIdx++) {
+			if (str[parenIdx] === '(') {
+				openParens++;
+			} else if (str[parenIdx] === ')') {
+				openParens--;
+				if (openParens === 0) {
+					return 'All open parens were closed, lastly at position: ' + parenIdx;
+				}
+			}
+		}
+	} else {
+		return 'No parens exist.';
+	}
+	throw Error('Unclosed parens.');
+}
+
+console.log(parenMatch('Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.')); // returns 79
+console.log(parenMatch('Some more ((open and closed) parens)')); // returns 35
+console.log(parenMatch('no parens')); // no parens
+console.log(parenMatch('(unclosed (parens)')); // unclosed parens
